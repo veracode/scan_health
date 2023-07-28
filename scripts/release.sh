aadb8a8416429ed9d297de2a61402f9f2cab4a2b
Anthony Fielding <ant@antfie.com>
2023-07-28 12:57:18 +0100

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,10 +7,13 @@ export VERSION="2.11"
 
 echo "${ESCAPE}[0;36mReleasing v${VERSION}...${ESCAPE}[0m" && \
 
-docker pull alpine
+docker pull alpine && \
+
 docker build -t antfie/scan_health:$VERSION . && \
 docker build -t antfie/scan_health . && \
 docker push antfie/scan_health:$VERSION && \
 docker push antfie/scan_health && \
+
+docker scout cves antfie/scan_health && \
 
 echo "${ESCAPE}[0;32mRelease Success${ESCAPE}[0m"
