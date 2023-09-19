258f1ff455ef339db93fdfbfdaf560daec2137ee
Anthony Fielding <ant@antfie.com>
2023-09-19 12:29:36 +0100

    Reduce precision of times for readability

--- utils/utils.go
+++ utils/utils.go
@@ -39,8 +39,16 @@ func FormatDuration(duration time.Duration) string {
 		str = fmt.Sprintf("%dd %dh%s", days, int(remainingHours), strings.Split(str, "h")[1])
 	}
 
-	str = strings.Replace(str, "h", "h ", 1)
-	str = strings.Replace(str, "m", "m ", 1)
+	// Deal with time units less than a minute, e.g. seconds, ms,μs,ns
+	if !strings.Contains(str, "m") {
+		return str
+	}
+
+	// Reduce precision of times for readability
+	str = strings.Split(str, "m")[0]
+
+	// Formatting
+	str = strings.Replace(str, "h", "h ", 1) + "m"
 
 	return str
 }
