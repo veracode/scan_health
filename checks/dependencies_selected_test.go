698d7e2d52ff69bc6eb8c11f9557ea793fd2bd98
Anthony Fielding <ant@antfie.com>
2023-09-20 14:03:24 +0100

    Add a unit test for dependencies selected

---
+++ checks/dependencies_selected_test.go
@@ -0,0 +1,43 @@
+package checks
+
+import (
+	"github.com/antfie/scan_health/v2/report"
+	"testing"
+)
+
+func TestDependenciesSelected(t *testing.T) {
+	t.Run("Unselected dependencies should not be reported", func(t *testing.T) {
+		mockReport := &report.Report{
+			Modules: []report.Module{
+				{Name: "Test",
+					Instances: []report.ModuleInstance{
+						{IsDependency: true},
+					}},
+			},
+		}
+
+		dependenciesSelected(mockReport)
+
+		if len(mockReport.Issues) != 0 {
+			t.Errorf("Issues were reported which should not have been")
+		}
+	})
+
+	t.Run("Selected dependencies should be reported", func(t *testing.T) {
+		mockReport := &report.Report{
+			Modules: []report.Module{
+				{Name: "Test",
+					Instances: []report.ModuleInstance{
+						{IsDependency: true},
+						{IsSelected: true},
+					}},
+			},
+		}
+
+		dependenciesSelected(mockReport)
+
+		if len(mockReport.Issues) != 1 {
+			t.Errorf("Issues were not which should have been")
+		}
+	})
+}
