9e6559f659264bca8f9e500d54cd578821b27932
Marcus Watson <mwatson@veracode.com>
2023-09-29 12:52:53 +0100

    More tests

---
+++ checks/unsupported_compiler_test.go
@@ -0,0 +1,108 @@
+package checks
+
+import (
+	"testing"
+
+	"github.com/antfie/scan_health/v2/report"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnsupportedCompiler(t *testing.T) {
+
+	t.Run("All Supported Modules", func(t *testing.T) {
+		t.Parallel()
+		mockReport := report.Report{
+			Modules: []report.Module{
+				{Name: "file3.exe",
+					IsIgnored:    false,
+					IsThirdParty: false,
+					Instances: []report.ModuleInstance{
+						{IsDependency: false},
+						{IsSelected: true},
+					}},
+			},
+		}
+
+		unsupportedPlatformOrCompiler(&mockReport)
+		assert.Empty(t, mockReport.Issues)
+	})
+
+	t.Run("Ignores third party files", func(t *testing.T) {
+		t.Parallel()
+		mockReport := report.Report{
+			Modules: []report.Module{
+				{Name: "file3.exe",
+					IsIgnored:    false,
+					IsThirdParty: true,
+					Instances: []report.ModuleInstance{
+						{IsDependency: false},
+						{IsSelected: true},
+					}},
+			},
+		}
+
+		unsupportedPlatformOrCompiler(&mockReport)
+		assert.Empty(t, mockReport.Issues)
+	})
+
+	t.Run("One unsupported module", func(t *testing.T) {
+		t.Parallel()
+		mockReport := report.Report{
+			Modules: []report.Module{
+				{Name: "bob.exe",
+					IsIgnored:    false,
+					IsThirdParty: false,
+					Instances: []report.ModuleInstance{
+						{IsDependency: false},
+						{IsSelected: true},
+						{HasFatalErrors: true},
+						{Status: "(Fatal)Unsupported Platform"},
+					}},
+			},
+		}
+
+		unsupportedPlatformOrCompiler(&mockReport)
+		if !assert.Equal(t, 1, len(mockReport.Issues)) {
+			t.FailNow()
+		}
+
+		assert.Contains(t, mockReport.Issues[0].Description, "A module could not be scanned")
+		assert.Equal(t, 1, len(mockReport.Recommendations))
+	})
+
+	t.Run("2 unsupported modules", func(t *testing.T) {
+		t.Parallel()
+		mockReport := report.Report{
+			Modules: []report.Module{
+				{Name: "bob.exe",
+					IsIgnored:    false,
+					IsThirdParty: false,
+					Instances: []report.ModuleInstance{
+						{IsDependency: false},
+						{IsSelected: true},
+						{HasFatalErrors: true},
+						{Status: "(Fatal)Unsupported Platform"},
+					}},
+
+				{Name: "bob2.exe",
+					IsIgnored:    false,
+					IsThirdParty: false,
+					Instances: []report.ModuleInstance{
+						{IsDependency: false},
+						{IsSelected: true},
+						{HasFatalErrors: true},
+						{Status: "(Fatal)Unsupported Compiler"},
+					}},
+			},
+		}
+
+		unsupportedPlatformOrCompiler(&mockReport)
+		if !assert.Equal(t, 1, len(mockReport.Issues)) {
+			t.FailNow()
+		}
+
+		assert.Contains(t, mockReport.Issues[0].Description, "2 modules could not be scanned")
+		assert.Equal(t, 1, len(mockReport.Recommendations))
+	})
+
+}
---
+++ checks/unwanted_files_test.go
@@ -0,0 +1,62 @@
+package checks
+
+import (
+	"testing"
+
+	"github.com/antfie/scan_health/v2/report"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test Cases
+func TestUnwantedFiles(t *testing.T) {
+
+	t.Run("No sensitive files", func(t *testing.T) {
+		t.Parallel()
+		testReport := report.Report{
+			UploadedFiles: []report.UploadedFile{
+				{Id: 111111, Name: "file1", MD5: "hash1", IsIgnored: false, IsThirdParty: false},
+				{Id: 222222, Name: "file2", MD5: "hash2", IsIgnored: false, IsThirdParty: false},
+			},
+			Issues: []report.Issue{},
+		}
+
+		detectUnwantedFiles(&testReport)
+		assert.Empty(t, testReport.Issues)
+	})
+
+	t.Run("Many Files, A Single Unwanted File", func(t *testing.T) {
+		t.Parallel()
+		testReport := report.Report{
+			UploadedFiles: []report.UploadedFile{
+				{Id: 111111, Name: "file1", MD5: "hash1", IsIgnored: false, IsThirdParty: false},
+				{Id: 222222, Name: "test.7z", MD5: "hash2", IsIgnored: false, IsThirdParty: false},
+			},
+			Issues: []report.Issue{},
+		}
+
+		detectUnwantedFiles(&testReport)
+
+		assert.Equal(t, 1, len(testReport.Issues))
+		assert.Contains(t, testReport.Issues[0].Description, "A 7-zip file was uploaded: \"test.7z\"")
+		assert.Equal(t, 2, len(testReport.Recommendations))
+	})
+
+	t.Run("Many Files, Many Unwanted Files", func(t *testing.T) {
+		t.Parallel()
+		testReport := report.Report{
+			UploadedFiles: []report.UploadedFile{
+				{Id: 111111, Name: "file1", MD5: "hash1", IsIgnored: false, IsThirdParty: false},
+				{Id: 222222, Name: "test.7z", MD5: "hash2", IsIgnored: false, IsThirdParty: false},
+				{Id: 222222, Name: "test.pyc", MD5: "hash2", IsIgnored: false, IsThirdParty: false},
+			},
+			Issues: []report.Issue{},
+		}
+
+		detectUnwantedFiles(&testReport)
+
+		assert.Equal(t, 2, len(testReport.Issues))
+		assert.Contains(t, testReport.Issues[0].Description, "A 7-zip file was uploaded: \"test.7z\"")
+		assert.Contains(t, testReport.Issues[1].Description, "A compiled Python file was uploaded: \"test.pyc\"")
+		assert.Equal(t, 3, len(testReport.Recommendations))
+	})
+}
