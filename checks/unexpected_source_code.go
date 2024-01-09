bad4ff34499a19f4cf843fb7f1ee37576b32822c
Marcus Watson <github@cus-productions.com>
2024-01-09 20:53:27 +0000

    Added swift to red flag source files

--- checks/unexpected_source_code.go
+++ checks/unexpected_source_code.go
@@ -16,6 +16,7 @@ func unexpectedSourceCode(r *report.Report) {
 	processSourceCodeFiles(r, []string{"*.csproj"}, "C# project file", []string{"Do not upload C# project files.", "Veracode requires .NET applications to be compiled with debug symbols into EXE, DLL or NUPKG files as per the packaging instructions: https://docs.veracode.com/r/compilation_net. Consider re-packaging these files such that they contain the actual application and not the source code."})
 	processSourceCodeFiles(r, []string{"*.c"}, "C source code file", []string{"Do not upload C source code. Veracode requires the application to be compiled with debug symbols."})
 	processSourceCodeFiles(r, []string{"*.cpp"}, "C++ source code file", []string{"Do not upload C++ source code. Veracode requires the application to be compiled with debug symbols."})
+	processSourceCodeFiles(r, []string{"*.swift"}, "Swift source code file", []string{"Do not upload Swift source code. Consider compiling these files as per the appropriate iOS packaging guidelines."})
 }
 
 func processSourceCodeFiles(r *report.Report, filePatterns []string, fileType string, recommendations []string) {
